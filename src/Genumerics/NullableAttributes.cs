b7a38ea701dd75e2a94a9d73396bd0f77d18bc40
TylerBrinkley <Tydude4Christ@hotmail.com>
2019-08-28 14:17:49 -0500

    Fix targeting .net core 3.0

--- src/Genumerics/NullableAttributes.cs
+++ src/Genumerics/NullableAttributes.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if !NULLABLE_ATTRIBUTES
 namespace System.Diagnostics.CodeAnalysis
 {
     ///// <summary>Specifies that null is allowed as an input even if the corresponding type disallows it.</summary>
@@ -86,3 +87,4 @@ namespace System.Diagnostics.CodeAnalysis
     //    public bool ParameterValue { get; }
     //}
 }
+#endif
